865ebcc05907e643c8b1a63808b7952e57e7d440
mitalidebnath191-gif <mitalidebnath191@gmail.com>
2026-05-27 10:03:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -25,13 +25,13 @@ URLSCAN_KEY = "019e52a8-932a-7662-9885-94e6a4673e31"
 
 fake = Faker()
 
-App Config
+#App Config
 
 st.set_page_config(page_title="OSINT Utility Tool", page_icon="⚡", layout="centered")
 st.title("⚡ OSINT & Utility Command Center")
 st.markdown("Type /help to see all available commands.")
 
-State init
+#State init
 
 if "messages" not in st.session_state:
 st.session_state.messages = []
@@ -254,13 +254,13 @@ req_data = ["QR", "TTS", "ENC", "DEC", "PWNED", "SHORT", "PING", "SUB", "WHOIS",
 except Exception as e:  
     return f"❌ Error: {str(e)}"
 
-Show Chat History
+#Show Chat History
 
 for message in st.session_state.messages:
 with st.chat_message(message["role"]):
 st.markdown(message["content"])
 
-User Input
+#User Input
 
 if user_input := st.chat_input("Enter command..."):
 st.session_state.messages.append({"role": "user", "content": user_input})
